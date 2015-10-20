46d4a196fe7e15eb7bd62c30060ba20c69c45a64
dayAlone <andrey.slider@gmail.com>
2015-10-21 00:30:06 +0500

    Mongo

--- config/test.js
+++ config/test.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { deferConfig as defer } from 'config/defer';
 
 current.domain = 'rh.radia.ru';
-//current.mongoose.uri = process.env.MONGOLAB_URI;
+current.mongoose.uri = process.env.MONGOLAB_URI;
 current.crypto.hash.iterations = 12000;
 current.folders = {
     source: defer(function(cfg) {

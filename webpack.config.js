03e5814ad43cc1d465afcff8c7591308241fcf39
dayAlone <andrey.slider@gmail.com>
2015-10-20 23:31:13 +0500

    Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
 require('dotenv').config({silent: true});
 require('babel/register');
-module.exports = process.env.NODE_ENV !== 'production' ? require('./webpack/development') : require('./webpack/production');
+module.exports = require(`./webpack/${process.env.NODE_ENV}`);

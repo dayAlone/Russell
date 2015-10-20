c4bc9ba6d29911046ea78302b94ec1a6cce99e27
dayAlone <andrey.slider@gmail.com>
2015-10-20 09:42:53 +0500

    Version

--- config/default.js
+++ config/default.js
@@ -4,7 +4,7 @@ import { deferConfig as defer } from 'config/defer';
 export default {
 
     cdn: `http://164623.selcdn.com/russell`,
-    version: process.env.VERSION || 'v1.1.6',
+    version: process.env.VERSION || 'v1.1.7',
     domain: 'localhost',
     __dirname: defer(function(cfg) {
         return cfg.root;

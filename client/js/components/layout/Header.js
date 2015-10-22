c99245fb055de5c2fc64ceb8af53cdf678c12e06
dayAlone <andrey.slider@gmail.com>
2015-10-22 14:27:40 +0500

    Modals

--- client/js/components/layout/Header.js
+++ client/js/components/layout/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import UserInfo from '../UserInfo'
 import { Link } from 'react-router'
-import Modal from 'boron/ScaleModal'
+import Modal from '../ui/Modal'
 import Nav from './Nav'
 class Header extends Component {
     openModal(e) {
@@ -11,6 +11,10 @@ class Header extends Component {
     openNav(e) {
         e.preventDefault()
         this.refs.nav.show()
+    }
+    closeNav(e) {
+        e.preventDefault()
+        this.refs.nav.hide()
     }
     componentDidUpdate() {
         this.refs.nav.hide()
@@ -43,19 +47,18 @@ class Header extends Component {
                     </div>
                 </div>
                 <Modal ref='nav' className='modal modal--nav'>
+                    <a href='#' className='modal__close' onClick={this.closeNav.bind(this)}><img src='/layout/images/svg/close.svg' alt='' width='20'/></a>
                     <Nav />
                 </Modal>
                 <Modal ref='modal' className='modal'>
-                    <div className='modal__content'>
-                        <div className='text'>
-                            <h2 className='modal__title'>Share the <img src='/layout/images/svg/heart.svg' width='26' /></h2>
-                            <p>Кто сказал, что от добра добра не ищут? Мы, Russell Hobbs, занимаемся этим вот уже больше 60 лет, и не только ищем, но и находим.</p>
-                            <p>Например, что может быть проще и удобней электрического чайника? Но мы взяли и создали первый чайник, который автоматическим отключался после закипани.</p>
-                            <p>Так же и с людьми. Вокруг много хороших людей, но мы решили найти и показать всем реальные факты, наглядно демонстрирующие масштаб людской доброты.</p>
-                            <p>И мы сделали это. Взяли и нашли множество жизненных историй, рассказывающих, как люди помогают друг другу просто потому, что они люди. Посмотрите их, пожалуйста.</p>
-                            <p>В жизни всегда есть место доброте и человечности. Давайте поделимся ими друг с другом. Приглашаем вас в путешествие, от добра к добру, от сердца к сердцу.</p>
-                            <p><strong>Ваш Russell Hobbs</strong></p>
-                        </div>
+                    <div className='text'>
+                        <h2 className='modal__title'>Share the <img src='/layout/images/svg/heart.svg' width='26' /></h2>
+                        <p>Кто сказал, что от добра добра не ищут? Мы, Russell Hobbs, занимаемся этим вот уже больше 60 лет, и не только ищем, но и находим.</p>
+                        <p>Например, что может быть проще и удобней электрического чайника? Но мы взяли и создали первый чайник, который автоматическим отключался после закипани.</p>
+                        <p>Так же и с людьми. Вокруг много хороших людей, но мы решили найти и показать всем реальные факты, наглядно демонстрирующие масштаб людской доброты.</p>
+                        <p>И мы сделали это. Взяли и нашли множество жизненных историй, рассказывающих, как люди помогают друг другу просто потому, что они люди. Посмотрите их, пожалуйста.</p>
+                        <p>В жизни всегда есть место доброте и человечности. Давайте поделимся ими друг с другом. Приглашаем вас в путешествие, от добра к добру, от сердца к сердцу.</p>
+                        <p><strong>Ваш Russell Hobbs</strong></p>
                     </div>
                 </Modal>
         </div>
--- client/js/components/ui/Modal.js
+++ client/js/components/ui/Modal.js
@@ -1,77 +1,99 @@
-import React, { Component } from 'react'
-//import Children from 'react-addons-children'
-import ScaleModal from 'boron/ScaleModal'
-class Modal extends Component {
-    handleOnHide() {
-        $('body')
-            .removeClass('modal-open')
-            .css('padding-right', 0)
-            .removeClass('modal-overflow')
-        return false
-    }
-    handleOnShow() {
-        $('body').addClass('modal-open')
+import React from 'react'
+import insertKeyframesRule from 'react-kit/insertKeyframesRule'
+import appendVendorPrefix from 'react-kit/appendVendorPrefix'
+import modalFactory from './modalFactory'
+var animation = {
+    show: {
+        animationDuration: '0.3s',
+        animationTimingFunction: 'cubic-bezier(0.6,0,0.4,1)'
+    },
+    hide: {
+        animationDuration: '0.3s',
+        animationTimingFunction: 'ease-out'
+    },
+    showContentAnimation: insertKeyframesRule({
+        '0%': {
+            opacity: 0,
+            transform: 'scale3d(0, 0, 1)'
+        },
+        '100%': {
+            opacity: 1,
+            transform: 'scale3d(1, 1, 1)'
+        }
+    }),
 
-        this.checkOwerflow()
-        $(window).on('resize', () => {
-            this.checkOwerflow()
-        })
-    }
+    hideContentAnimation: insertKeyframesRule({
+        '0%': {
+            opacity: 1
+        },
+        '100%': {
+            opacity: 0,
+            transform: 'scale3d(0.5, 0.5, 1)'
+        }
+    }),
 
-    measureScrollbar() {
-        let scrollDiv = document.createElement('div')
-        $('body').append(scrollDiv)
+    showBackdropAnimation: insertKeyframesRule({
+        '0%': {
+            opacity: 0
+        },
+        '100%': {
+            opacity: 1
+        },
+    }),
 
-        $(scrollDiv).css({ position: 'absolute', top: '-9999px', width: '50px', height: '50px', overflow: 'scroll' })
-        let scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth
-        $(scrollDiv).remove()
-
-        return scrollbarWidth
-    }
-    checkOwerflow() {
-        setTimeout(() => {
-            const scrollbarWidth = this.measureScrollbar()
-            const bodyPadding = parseInt(($('body').css('padding-right') || 0), 10)
-            const isOverflow = $('.modal__content').height() > $('.modal-frame').height()
-            if (isOverflow) {
-                $('body')
-                    .css('padding-right', bodyPadding + scrollbarWidth)
-                    .addClass('modal-overflow')
-            } else {
-                $('body')
-                    .css('padding-right', 0)
-                    .removeClass('modal-overflow')
-            }
-        }, 500)
-    }
-
-    show() {
-        this.refs.modal.enter()
-        this.refs.modal.show()
-    }
-    hide() {
-        this.refs.modal.hide()
-    }
-    componentWillUnmount() {
-        this.handleOnHide()
-    }
-    renderChildren() {
-        return React.Children.map(this.props.children, child => {
-            return React.cloneElement(child, {
-                hideModal: this.hide,
-                hello: 123
-            })
-        })
-    }
-    render() {
-        return <div className={`modal-frame ${this.props.static ? 'modal-frame--static' : ''}`}>
-            <ScaleModal {...this.props}
-                className={'modal ' + this.props.className}
-                ref='modal'
-                onHide={this.handleOnHide.bind(this)}
-                onShow={this.handleOnShow.bind(this)}/>
-        </div>
-    }
+    hideBackdropAnimation: insertKeyframesRule({
+        '0%': {
+            opacity: 1
+        },
+        '100%': {
+            opacity: 0
+        }
+    })
 }
 
-export default Modal
+let {
+    show: showAnimation,
+    hide: hideAnimation,
+    showContentAnimation,
+    hideContentAnimation,
+    showBackdropAnimation,
+    hideBackdropAnimation,
+} = animation
+
+export default modalFactory({
+    getRef() {
+        return 'content'
+    },
+    getModalStyle() {
+        return appendVendorPrefix({
+            zIndex: 1050,
+            position: 'fixed',
+            transform: 'translate3d(-50%, -50%, 0)',
+            top: '50%',
+            left: '50%'
+        })
+    },
+    getBackdropStyle(willHidden) {
+        return appendVendorPrefix({
+            position: 'fixed',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            zIndex: 1040,
+            animationFillMode: 'forwards',
+            animationDuration: '0.4s',
+            animationName: willHidden ? hideBackdropAnimation : showBackdropAnimation,
+            animationTimingFunction: (willHidden ? hideAnimation : showAnimation).animationTimingFunction
+        })
+    },
+    getContentStyle(willHidden) {
+        return appendVendorPrefix({
+            margin: 0,
+            animationDuration: (willHidden ? hideAnimation : showAnimation).animationDuration,
+            animationFillMode: 'forwards',
+            animationName: willHidden ? hideContentAnimation : showContentAnimation,
+            animationTimingFunction: (willHidden ? hideAnimation : showAnimation).animationTimingFunction
+        })
+    }
+})

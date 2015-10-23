0cd2bc4806e8abb025217217c6eaf75873602d28
dayAlone <andrey.slider@gmail.com>
2015-10-23 13:35:54 +0500

    Controllers

---
+++ server/controllers/300-receps/index.js
@@ -0,0 +1,20 @@
+import Recept from '../../models/recepts'
+import Router from 'koa-router'
+
+export default function(app) {
+    const router = new Router()
+
+    router
+        .get('/recepts/get/', function*() {
+            let result
+            try {
+                result = yield Recept.find()
+            } catch (e) {
+                this.body = { error: e }
+            }
+            this.set('Cache-Control', 'max-age=3600000, must-revalidate')
+            this.body = { error: false, result: result }
+        })
+    console.log(router.routes())
+    app.use(router.routes())
+}
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,16 +1,17 @@
-import fs from 'fs';
+import fs from 'fs'
 const excludes = [
     'index.js'
-];
+]
 
 export default (app) => {
     const files = fs.readdirSync(__dirname)
-        .filter(file => { return !excludes.includes(file); })
-        .map(file => { return file.replace('.js', ''); })
-        .sort();
-
+        .filter(file => { return !excludes.includes(file) })
+        .map(file => { return file.replace('.js', '') })
+        .sort((a, b)=>{
+            return parseInt(a.match(/(\d{3,4})-/), 10) - parseInt(b.match(/(\d{3,4})-/), 10)
+        })
     files.forEach(file => {
-        require(`./${file}`)(app);
-    });
+        require(`./${file}`)(app)
+    })
 
-};
+}

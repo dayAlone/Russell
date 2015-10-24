47ca75b26de5d950633cab245467fbfbcaf79046
dayAlone <andrey.slider@gmail.com>
2015-10-25 00:15:27 +0500

    Changes

--- server/middlewares/650-auth.js
+++ server/middlewares/650-auth.js
@@ -1,18 +1,18 @@
-import auth from 'koa-basic-auth';
-import config from 'config';
+import auth from 'koa-basic-auth'
+import config from 'config'
 
-var env = process.env.NODE_ENV || 'test';
-const access = config.get('basicAuth');
+const access = config.get('basicAuth')
 
 const tryAuth = function* (next) {
-    try { yield next; }
-    catch (err) {
+    try {
+        yield next
+    } catch (err) {
         if (401 === err.status) {
-            this.status = 401;
-            this.set('WWW-Authenticate', 'Basic');
-            this.body = 'Nope... you need to authenticate first. With a proper user!';
-        } else { throw err; }
+            this.status = 401
+            this.set('WWW-Authenticate', 'Basic')
+            this.body = 'Nope... you need to authenticate first. With a proper user!'
+        } else { throw err }
     }
-};
+}
 
-export default process.env.HIDE === 'Y' ? [tryAuth, auth({name: access.name, pass: access.pass})] : null;
+export default process.env.HIDE === 'Y' ? [tryAuth, auth({name: access.name, pass: access.pass})] : null

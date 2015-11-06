36e077f1aad1730c4569d27017f2966e789888ec
dayAlone <andrey.slider@gmail.com>
2015-11-06 23:37:39 +0500

    Changes

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
 import current from './default';
-current.domain = 'russellhobbs-promo.com';
+current.domain = 'russellhobbs-promo.ru';
 current.mongoose.uri = process.env.MONGOLAB_URI;
 current.crypto.hash.iterations = 12000;
 export default current;

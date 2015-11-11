4025edfbad54699051295b60d161b5a2788ec58f
dayAlone <andrey.slider@gmail.com>
2015-11-12 01:53:18 +0500

    Time

--- server/controllers/150-games/index.js
+++ server/controllers/150-games/index.js
@@ -269,7 +269,7 @@ export default function(app) {
                 let result
                 let {type, finished} = this.request.body
                 let now = new time.Date()
-                now.setTimezone('Etc/GMT+10')
+                now.setTimezone('Europe/Moscow')
                 try {
 
                     result = yield getUserScores(this.req.user, function*(user) {

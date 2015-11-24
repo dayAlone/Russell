a15f5e775805c292fb8cbd1ab9a2b0d215666327
dayAlone <andrey.slider@gmail.com>
2015-11-24 20:54:18 +0500

    Dream

--- client/js/components/games/Dream.js
+++ client/js/components/games/Dream.js
@@ -53,8 +53,7 @@ class Dream extends Component {
                 <img src={`/layout/images/line.png`} width='100%' className='text__divider' height='2'/>
                 <div className='center'>
                     {this.props.isLogin ? <Link to='/profile/checks/' className='button'>Принять участие</Link> : <a onClick={this.openModal.bind(this)} href='#' className='button'>Принять участие</a>}<br/>
-                    { this.state.more ? null : <a href='#' onClick={this.toggleMore(true)}>Подробнее</a><br/>
-                    <div><small><a href='http://164623.selcdn.com/russell/rules/Vyigray_mechtu_rules.pdf' target='_blank'>Условия и правила проведения акции</a></small></div> }
+                { this.state.more ? null : <span><a href='#' onClick={this.toggleMore(true)}>Подробнее</a><br/><small><a href='http://164623.selcdn.com/russell/rules/Vyigray_mechtu_rules.pdf' target='_blank'>Условия и правила проведения акции</a></small></span> }
                 </div>
                 { this.state.more ? <div className='dream__more'>
                     <p>В акции могут участвовать чеки от покупок, сделанных в период с 12 октября по 27 декабря.</p>
@@ -84,7 +83,7 @@ class Dream extends Component {
                     </section>
                     <p>После определения победителей каждого розыгрыша, победителю необходимо разместить свои контактные данные в его Личном кабинете на нашем сайте, для получения к ним доступа администратора акции.</p>
 
-                    
+
                     <div className='center'>
                         <p><a href='#' onClick={this.toggleMore(false)}><a href='#'>Свернуть</a></a></p>
                     </div>

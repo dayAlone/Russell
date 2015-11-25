85c498e0a263be38165a91afb252603b54b9515f
dayAlone <andrey.slider@gmail.com>
2015-11-25 12:54:54 +0500

    CSS

--- client/js/components/games/Present.js
+++ client/js/components/games/Present.js
@@ -174,7 +174,7 @@ class Step2 extends Component {
                                 ref='cropper'
                                 src={file}
                                 data={this.state.cropper}
-                                style={{height: 600, width: 600}}
+                                style={{minHeight: 600}}
                                 aspectRatio={1 / 1}
                                 guides={false}
                                 minCropBoxHeight={200}

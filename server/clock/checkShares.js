da9c0779035a8c7d3296a792af2f8c636c348604
dayAlone <andrey.slider@gmail.com>
2015-12-04 17:41:25 +0500

    Changes

--- server/clock/checkShares.js
+++ server/clock/checkShares.js
@@ -3,6 +3,8 @@ import Scores from '../models/scores'
 import config from 'config'
 import request from 'co-request'
 import moment from 'moment'
+import vk from 'vksdk'
+
 
 export default function* () {
     try {

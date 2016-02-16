acec61820f5bc598f307f95ec92decdbf15977b5
dayAlone <andrey.slider@gmail.com>
2016-02-16 19:39:15 +0500

    Domain

--- server/middlewares/50-domain.js
+++ server/middlewares/50-domain.js
@@ -1,5 +1,6 @@
 import config from 'config'
 export default function*(next) {
+    console.log(config.domain, this.request.host)
     if ('www.' + config.domain !== this.request.host) {
         this.response.redirect('http://www.' + config.domain)
     }

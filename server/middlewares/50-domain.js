b72765d6121a07f0fa195c196f9cfbdb690a1a7d
dayAlone <andrey.slider@gmail.com>
2016-02-16 19:48:30 +0500

    Domain

--- server/middlewares/50-domain.js
+++ server/middlewares/50-domain.js
@@ -1,8 +1,7 @@
 import config from 'config'
 export default function*(next) {
-    console.log(config.domain, this.request.host)
-    if ('www.' + config.domain !== this.request.host) {
-        this.response.redirect('http://www.' + config.domain)
+    if (config.domain !== this.request.host) {
+        this.response.redirect('http://' + config.domain)
     }
     yield next
 }
